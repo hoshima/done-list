5d87f25f5af567d2d7d44ed930ae3abbc5a9a268
hoshima <hoshima@users.noreply.github.com>
2024-03-05 20:39:29 +0900

    formにバリデーションを追加

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CreateListItem } from '../../interfaces/list-item';
-import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import {
+  MatError,
+  MatFormField,
+  MatInput,
+  MatLabel,
+} from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { cdate } from 'cdate';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule, MatFormField, MatInput, MatLabel, MatButton],
+  imports: [FormsModule, MatFormField, MatInput, MatLabel, MatButton, MatError],
   templateUrl: './form.component.html',
   styles: `
     :host {
