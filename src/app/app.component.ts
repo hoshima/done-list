30e8224744dcbc9ae6565483449f847c8b4d1b9b
hoshima <hoshima@users.noreply.github.com>
2024-03-09 00:16:25 +0900

    ページヘッダーを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIcon, MatIconButton, MatToolbar],
   templateUrl: './app.component.html',
   styles: [],
 })
