5456060b0e9871525b9511132057483cd424c033
hoshima <hoshima@users.noreply.github.com>
2024-03-17 01:01:43 +0900

    chore: :wrench: development環境のenvファイルを設定
    いったんprod環境と同じものにする

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'done-list-58de5',
+    appId: '1:404826449834:web:1c6ba0e8d00c17a7592dd3',
+    storageBucket: 'done-list-58de5.appspot.com',
+    apiKey: 'AIzaSyBsBqqbfqZrhlBT8rhb24ym4gp4oerFDy8',
+    authDomain: 'done-list-58de5.firebaseapp.com',
+    messagingSenderId: '404826449834',
+    measurementId: 'G-S8YSBN32F5',
+  },
+};
