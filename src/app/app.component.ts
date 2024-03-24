156ff78c6a95fdd5bc51cc8fe6fbb70cd0dc88fc
hoshima <hoshima@users.noreply.github.com>
2024-03-24 20:45:36 +0900

    feat: :sparkles: mat-drawerを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIcon, MatIconButton, MatToolbar],
+  imports: [RouterOutlet, MatIcon, MatIconButton, MatToolbar, MatSidenavModule],
   templateUrl: './app.component.html',
-  styles: [],
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: { class: 'block h-svh' },
 })
 export class AppComponent {
   title = 'done-list';
