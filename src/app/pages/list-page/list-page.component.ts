ec487452af4c75615a88f044486742af872f883d
hoshima <hoshima@users.noreply.github.com>
2024-09-25 20:26:29 +0900

    feat: :fire: elf上のTasksRepositoryを削除

--- src/app/pages/list-page/list-page.component.ts
+++ src/app/pages/list-page/list-page.component.ts
@@ -6,7 +6,6 @@ import { MatIcon } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskFormComponent } from '../../components/task-form/task-form.component';
 import { filter } from 'rxjs';
-import { TasksRepository } from '../../states/tasks.repository';
 import { AsyncPipe } from '@angular/common';
 import { FirestoreService } from '../../services/firestore.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -24,10 +23,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export default class ListPageComponent {
   readonly #dialog = inject(MatDialog);
   readonly #snackBar = inject(MatSnackBar);
-  readonly #repo = inject(TasksRepository);
   readonly #firestoreService = inject(FirestoreService);
-
-  tasks$ = this.#repo.tasks$;
 
   onDeleteItem([id, name]: [string, string]) {
     if (!confirm(`${name}を削除しますか？`)) {
@@ -59,13 +55,6 @@ export default class ListPageComponent {
       .open<TaskFormComponent, null, CreateListItem>(TaskFormComponent)
       .afterClosed()
       .pipe(filter((x): x is Exclude<typeof x, undefined> => x != null))
-      .subscribe((task) => {
-        this.#repo.addTask({
-          id: crypto.randomUUID(),
-          ...task,
-        });
-
-        this.#firestoreService.addTask(task);
-      });
+      .subscribe((task) => this.#firestoreService.addTask(task));
   }
 }
