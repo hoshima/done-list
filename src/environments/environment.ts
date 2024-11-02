f631a631d6894fb6efbc53321dc1a379d0df8a2e
hoshima <hoshima@users.noreply.github.com>
2024-11-03 02:12:27 +0900

    chore: :wrench: supabaseのAPI Keyを設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,4 +12,9 @@ export const environment = {
   reCAPTCHA: {
     siteKey: '6LfAZVgqAAAAALLVrIbU-oez_d2i6xksP7QhGMXd',
   },
+  supabase: {
+    supabaseUrl: 'https://igfboflygtixuqmmlagm.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnZmJvZmx5Z3RpeHVxbW1sYWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NDM0ODgsImV4cCI6MjA0NjAxOTQ4OH0.X6TWD7JbASHvWtQLxfWpFppTaYYouQbSz3G8V7S-G2M',
+  },
 };
