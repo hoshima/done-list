0f75709e2ece4b1b2869c377594abfc073aff5dd
hoshima <hoshima@users.noreply.github.com>
2024-11-03 11:09:28 +0900

    feat: :coffin: LoginComponentの不要なコードを削除

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,33 +1,14 @@
-import { NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthComponent } from '../../components/auth/auth.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgOptimizedImage, AuthComponent],
+  imports: [AuthComponent],
   templateUrl: './login.component.html',
   host: {
     class: 'block container mx-auto',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LoginComponent {
-  #authService = inject(AuthService);
-  #router = inject(Router);
-
-  constructor() {
-    this.#authService.user$.pipe(takeUntilDestroyed()).subscribe((user) => {
-      if (user) {
-        this.#router.navigateByUrl('');
-      }
-    });
-  }
-
-  async loginWithGoogle() {
-    await this.#authService.login();
-  }
-}
+export default class LoginComponent {}
