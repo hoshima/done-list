7c91182b03de130d21211b9333000b6de85ceb46
hoshima <hoshima@users.noreply.github.com>
2024-11-29 17:17:46 +0900

    perf: :zap: sidemenuの軽量化を試みる
    遅延読み込みとimportの軽量化

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatActionList, MatListItem } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbar } from '@angular/material/toolbar';
 import { UiRepository } from '../../states/ui.repository';
 import { ExternalFileService } from '../../services/external-file.service';
 import { SupabaseService } from '../../services/supabase.service';
@@ -11,7 +11,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-side-menu',
   imports: [
-    MatToolbarModule,
+    MatToolbar,
     MatIcon,
     MatIconButton,
     MatActionList,
